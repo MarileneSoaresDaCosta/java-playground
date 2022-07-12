a588c3c5bacd1aa09836b25a70c34370d7af1187
Marilene Soares da Costa <lene626@gmail.com>
2022-07-12 15:38:50 -0600

    adds fizzbuzz

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,9 +3,36 @@ import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
-//        System.out.println("Hello World!");
+    static void fizzBuzz() {
+        System.out.println("This is fizzBuzz");
+        Scanner userObj = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        String userNum = userObj.nextLine();
+        int num = Integer.parseInt(userNum);
 
+        // fizzBuzz logic
+        String result= "";
+        if(num % 3 == 0) {
+            result += "Fizz";
+        }
+
+        if (num % 5 == 0) {
+            result += "Buzz";
+        }
+
+        if(num % 3 != 0 && num % 5 != 0) {
+            System.out.println(num);
+        } else {
+            System.out.println(result);
+        }
+//        System.out.println(String.format("You chose %d", num));
+
+    }
+    public static void main(String[] args) {
+
+        fizzBuzz();
+
+        /*
         System.out.println(String.format("num of args: %d", args.length));
         if(args.length < 1) {
             System.out.println("Please provide an argument!");
@@ -17,5 +44,8 @@ public class Application {
         System.out.println("Enter username:");
         String userName = myObj.nextLine();
         System.out.println(String.format("Hello %s", userName));
+         */
+
+
     }
 }
