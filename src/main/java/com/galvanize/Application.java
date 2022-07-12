5fafeeee071155f6d6764df4efad7a84d5fc6bca
Marilene Soares da Costa <lene626@gmail.com>
2022-07-12 13:54:15 -0600

    reading arguments

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,8 +1,21 @@
 package com.galvanize;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+
+        System.out.println(String.format("num of args: %d", args.length));
+        if(args.length < 1) {
+            System.out.println("Please provide an argument!");
+        } else {
+            System.out.println(String.format("Argument provided: %s", args[0]));
+        }
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username:");
+        String userName = myObj.nextLine();
+        System.out.println(String.format("Hello %s", userName));
     }
 }
