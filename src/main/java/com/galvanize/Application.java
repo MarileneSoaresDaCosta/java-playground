9a4793712ff93ab8e86ba10d27a116103120af73
Marilene Soares da Costa <lene626@gmail.com>
2022-07-12 14:02:23 -0600

    task completed

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) {
-//        System.out.println("Hello World!");
 
-        System.out.println(String.format("num of args: %d", args.length));
-        if(args.length < 1) {
-            System.out.println("Please provide an argument!");
+        if(args.length < 2) {
+            System.out.println("Please provide name and email.");
+            System.exit(0);
         } else {
-            System.out.println(String.format("Argument provided: %s", args[0]));
+            String name = args[0];
+            String email = args[1];
+            System.out.println(String.format("%s <%s>", name, email));
         }
 
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter username:");
-        String userName = myObj.nextLine();
-        System.out.println(String.format("Hello %s", userName));
     }
 }
